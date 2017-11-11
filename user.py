699148be8e81bef176edc3351c05d057dba1b7a6
beatrizaraujo1 <33091171+beatrizaraujo1@users.noreply.github.com>
2017-11-11 12:34:01 -0300

    Update user.py

--- user.py
+++ user.py
@@ -9,31 +9,26 @@ class User:
 
     def get_nome(self):
         return self.nome
-
     def set_nome(self, novo_nome):
         self.nome = novo_nome
 
     def get_endereco(self):
         return self.endereco
-
     def set_endereco(self, novo_endereco):
         self.endereco = novo_endereco
 
     def get_data_nsc(self):
         return self.data_nsc
-
     def set_data_nsc(self, novo_data_nsc):
         self.data_nsc = novo_data_nsc
 
     def get_telefone(self):
         return self.telefone
-
     def set_telefone(self, novo_telefone):
         self.telefone = novo_telefone
 
     def set_email(self, novo_email):
         self.email = novo_email
-
     def set_senha(self, nova_senha):
         self.senha = nova_senha
 
@@ -47,32 +42,18 @@ class User:
         opcao = input('Digite a opção:')
 
         if opcao == '1':
-            novo_nome = input(str('Digite um nome:'))  # coigir o uso da fucão set baseado em outro codigos
-            set_nome(novo_nome)
+            novo_nome = input(str('Digite um nome:'))
+            self.set_nome(novo_nome)
         if opcao == '2':
             novo_end = input(str('Digite seu endereço:(Rua, Número e Cidade)'))
-
+            self.set_endereco(novo_end)
         if opcao == '3':
-            nova_data = input(str('Insira sua data de nascimento:(dd/mm/aa)'))
-
+            nova_data = input('Insira sua data de nascimento:(dd/mm/aa)')
+            self.set_data_nsc(nova_data)
         if opcao == '4':
             novo_tel = input(str('Digite se número de telefone:(+xx(ddd)xxxx-xxxx)'))
-
+            self.set_telefone(novo_tel)
         if opcao == '5':
-            print('Todos os Dados:' / next('Nome:', get_nome()) / next('Endereço:', )  # verificar utilização do get
+            print('Todos os Dados:' / next('Nome:', self.get_nome()) / next('Endereço:', self.get_endereco()) / next('Data de Nascimento:', self.get_data_nsc())/ next('Telefone:', self.get_telefone()))
 
     return opcao
-
-    def logar(self):
-        resposta = str
-        while (resposta != 'loguin efetuado'):
-            e = input("Insira o email:")
-            s = input("Senha:")
-            if e == self.email and s == self.senha:
-                print('Loguin efetuado')
-                # chamar menu de opções
-                # criar
-
-            else:
-                print('email ou senha incorretos')
-                respoata = ('email ou senha incorretos')
