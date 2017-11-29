807eddc7a0d03adaac773045b11ea317a0302ff1
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2017-11-29 20:44:12 -0200

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -1,4 +1,16 @@
-class Cadastrar:
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Crie o banco de dados em sqlite e salva no arquivo: /tmp/test.db
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Cadastrar(db.Model):
+        
+    email = db.Column(db.String(100), primary_key=True)
+    senha = db.Column(db.String(18))
+
     def __init__(self):
         self.lista_email = []
         self.lista_senha = []
@@ -9,3 +21,6 @@ class Cadastrar:
         Senha = input('Digite uma senha')
         self.lista_email.append(Loguin)
         self.lista_senha.append(Senha)
+
+db.create_all()
+db.session.add()
