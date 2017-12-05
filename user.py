749e61c2512b67ddefbe27432c3531e41d038794
Ana Beatriz de Araujo Farias <33091171+beatrizaraujo1@users.noreply.github.com>
2017-12-05 13:01:27 -0200

    Update user.py

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__email_senha__)
+app = Flask(__user__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
