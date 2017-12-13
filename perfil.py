8edad2929b931b0ed145b949bc86f37a85f780fa
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2017-12-12 23:46:55 -0300

    Update perfil.py

--- perfil.py
+++ perfil.py
@@ -1,16 +1,11 @@
 class Perfil:
     def __init__(self,foto,bibliografia):
-        self.foto = foto
         self.biblio = bibliografia
-
-    def get_foto(self):
-        return self.foto
-    def set_foto(self, nova_foto):
-        self.foto = nova_foto
-
     def get_biblio(self):
         return self.biblio
     def set_biblio(self, nova_biblio):
         self.biblio = nova_biblio
-#colocar em usuario
-#tipo foto é o blob
+        
+
+
+
