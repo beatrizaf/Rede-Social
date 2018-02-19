6cf1ccc68ea4b1f120c641ac889199d17ef73122
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2018-02-19 02:05:16 -0300

    Update feed.py

--- feed.py
+++ feed.py
@@ -1,4 +1,6 @@
-from batepapo import Batepapo
+from batepapo import *
+from acounts import *
+log = Bd()
 bp = Batepapo()
 class Perfil:
     def __init__(self):
@@ -6,12 +8,13 @@ class Perfil:
         self.amigos = list
         self.notificacao = list
 
-    def menu_feed(self,email,senha):
+    def menu_feed(self,emai,senh,n,i,t,e):
         print('\n\n')
         print('Bem vindo')
         print('1 - Editar Bibliografia')
         print('2 - Abrir bate papo')
         print('3 - Ver perfil')
+        print('4 - Mudar email e senha')
         print('x - Sair')
         opc = input('Digite a opção: ')
 
@@ -22,12 +25,20 @@ class Perfil:
             pass
             opca = ''
             while opca != 'x':
-               opca = bp.menu(None)
+               opca = bp.menu()
         if opc == '3':
+            perf = log.contas
             print(self.bibliografia)
             c = 0
             for x in self.amigos():
                 c+=1
+                print(perf.nome,perf.idade,perf.telefone,perf.endereco)
             print('amigos =',c)
-
+        if opc == '4':
+            for x in log.loguin.email:
+                if x == emai:
+                    log.loguin.email[x] = input('digite seu novo email: ')
+            for y in log.loguin.senha:
+                if y == senh:
+                   log.loguin.senha[y] = input('digite sua nova senha: ')
         return(opc)
